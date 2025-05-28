baea071a41a3852937f699bd8b3d0136082ba710
warkad12 <poonam@nuagecx.com>
2025-05-28 16:32:42 +0530

    cclimk.js

--- cclimk.js
+++ cclimk.js
@@ -1,24 +1,25 @@
-var crypto = require('crypto');
-exports.encrypt = function (plainText, workingKey) {
-	var m = crypto.createHash('md5');
-    	m.update(workingKey);
-   	var key = m.digest();
-      	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';	
-	var cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
-	var encoded = cipher.update(plainText,'utf8','hex');
-	encoded += cipher.final('hex');
-    	return encoded;
-};
+const crypto = require('crypto');
 
+exports.encrypt = function (plainText, workingKey) {
+    const key = crypto.createHash('md5').update(workingKey).digest();
+    const iv = Buffer.from([0x00, 0x01, 0x02, 0x03,
+                            0x04, 0x05, 0x06, 0x07,
+                            0x08, 0x09, 0x0a, 0x0b,
+                            0x0c, 0x0d, 0x0e, 0x0f]);
+    const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+    let encrypted = cipher.update(plainText, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+};
 
 exports.decrypt = function (encText, workingKey) {
-    	var m = crypto.createHash('md5');
-    	m.update(workingKey)
-    	var key = m.digest();
-	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';	
-	var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-    	var decoded = decipher.update(encText,'hex','utf8');
-	decoded += decipher.final('utf8');
-    	return decoded;
+    const key = crypto.createHash('md5').update(workingKey).digest();
+    const iv = Buffer.from([0x00, 0x01, 0x02, 0x03,
+                            0x04, 0x05, 0x06, 0x07,
+                            0x08, 0x09, 0x0a, 0x0b,
+                            0x0c, 0x0d, 0x0e, 0x0f]);
+    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+    let decrypted = decipher.update(encText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
 };
-
